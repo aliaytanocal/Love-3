eb659a15d981e97bb5f1cffd5275ad24aca8b1af
Ali Aytan Öçal <36601746+aliaytanocal@users.noreply.github.com>
2022-11-22 23:13:37 +0300

    Add files via upload

---
+++ love.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+screen = Screen()
+
+title("Love")
+bgcolor("black")
+color("red")
+begin_fill()
+pensize(4)
+left(50)
+forward(130)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
+screen.mainloop()
